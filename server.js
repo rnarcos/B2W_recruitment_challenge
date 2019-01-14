666b57d20c032968c024a415ed75aed2c0b983b5
Marcos Fernandes <rnvrcos@gmail.com>
2019-01-14 17:59:45 -0200

    Remodling a few operations
    This was a general cleanup with better practices and such.

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const port = 2000;
-
+const port = process.env.NODE_SERVER_PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port);
